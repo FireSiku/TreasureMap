c12f50d43cd882c0490c0323fc58af44af355811
FireSiku <chaoslux@gmail.com>
2017-09-05 06:17:50 -0400

    Now it starts to look like something. Still using placeholder data though.

--- TreasureMap.lua
+++ TreasureMap.lua
@@ -11,6 +11,8 @@
 ---------------------
 local addonName = ...
 local addon = LibStub("AceAddon-3.0"):NewAddon(addonName, "AceEvent-3.0")
+local HBD = LibStub("HereBeDragons-1.0")
+local HBDPins = LibStub("HereBeDragons-Pins-1.0")
 local options = {}
 local db
 
@@ -21,9 +23,163 @@ local defaults = {
 	},
 }
 
+local continentList = {}
+
 ---------------------
 -- Addon Functions --
---------------------- 
+---------------------
+
+local pinTable = {
+	{ name = "Icecrown Test"   , x = 62, y = 40, id = 492, }, 
+	{ name = "Storm Peaks Test", x = 62, y = 40, id = 495, }, 
+	{ name = "ICC Test 1"      , x = 53, y = 86, id = 492, group = "ICC"   , }, 
+	{ name = "ICC Test 2"      , x = 53, y = 86, id = 492, group = "ICC"   , }, 
+	{ name = "ICC Test 3"      , x = 53, y = 86, id = 492, group = "ICC"   , }, 
+	{ name = "ICC Test 4"      , x = 53, y = 86, id = 492, group = "ICC"   , }, 
+	{ name = "ICC Test 5"      , x = 53, y = 86, id = 492, group = "ICC"   , }, 
+	{ name = "ICC Test 6"      , x = 53, y = 86, id = 492, group = "ICC"   , }, 
+	{ name = "ICC Test 7"      , x = 53, y = 86, id = 492, group = "ICC"   , }, 
+	{ name = "ICC Test 8"      , x = 53, y = 86, id = 492, group = "ICC"   , }, 
+	{ name = "UlduarTest 1"    , x = 41, y = 18, id = 495, group = "Ulduar", }, 
+	{ name = "UlduarTest 2"    , x = 41, y = 18, id = 495, group = "Ulduar", }, 
+	{ name = "UlduarTest 3"    , x = 41, y = 18, id = 495, group = "Ulduar", }, 
+	{ name = "UlduarTest 4"    , x = 41, y = 18, id = 495, group = "Ulduar", }, 
+	{ name = "UlduarTest 5"    , x = 41, y = 18, id = 495, group = "Ulduar", }, 
+	{ name = "UlduarTest 6"    , x = 41, y = 18, id = 495, group = "Ulduar", }, 
+	{ name = "UlduarTest 7"    , x = 41, y = 18, id = 495, group = "Ulduar", },
+
+}
+
+local function CreatePin(name)
+	local pin = CreateFrame("Button", name, WorldMapButton)
+	pin:SetSize(20,20)
+	pin:SetPoint("CENTER", WorldMapButton, "CENTER")
+
+	local tex = pin:CreateTexture(nil, "OVERLAY")
+	tex:SetAllPoints(pin)
+	tex:SetTexture("Interface\\RAIDFRAME\\ReadyCheck-NotReady.blp")
+	--tex:SetDesaturated(true)
+
+	pin.texture = tex
+	pin:Show()
+	return pin
+end
+
+local pinGroupStorage = {}
+local function GetPinGroup(name)
+	if pinGroupStorage[name] then 
+		return pinGroupStorage[name] 
+	end
+
+	-- If it doesn't exists, create it.
+	local group = CreateFrame("Frame", "TMGRoup"..name, WorldMapButton)
+	pinGroupStorage[name] = group
+	group:SetSize(20,20)
+	return group
+end
+
+local function UpdatePinGroupAnchors(name)
+	local group = pinGroupStorage[name]
+
+	group[1]:SetPoint("TOPLEFT", group, "TOPLEFT")
+	for i = 2, #group do
+		group[i]:SetPoint("LEFT", group[i-1], "RIGHT", -5, 0)
+	end
+end
+
+function addon:AddWorldMapPin(mapID, x, y)
+	--local pinName = "TMTest"..random(1,100)
+	local pinName = "TMTest"
+	local pin = CreatePin(pinName)
+	LUI:Print("Added Pin:",pinName)
+	-- Coords seen ingame and on wowhead are multiplied by 100 to be human-readable. 
+	HBDPins:AddWorldMapIconMF(addonName, pin, mapID, nil, x/100, y/100)
+	pin:Show()
+end
+
+local X_SPACING = 1.5
+local Y_SPACING = 2.0
+local SPACING = 2
+function addon:UpdateGroupedPinPosition(name)
+	local group = pinGroupStorage[name]
+	if not group then return end
+
+	-- this will let for a roughly even distribution of pins in a square formation
+	local rowSize = ceil(sqrt(#group)) 
+	local offsetX, offsetY = 0, 0
+	local index = 0
+	for i = 1, #group do
+		index = index + 1
+		local pin = group[i]
+		local x = pin.x + offsetX
+		local y = pin.y + offsetY
+		HBDPins:AddWorldMapIconMF(addonName, pin, pin.id, nil, x/100, y/100)
+
+		--Calculate offset for next pin
+		if index == rowSize then
+			index = 0
+			offsetX = 0
+			offsetY = offsetY + Y_SPACING
+		else
+			offsetX = offsetX + X_SPACING
+		end
+
+	end
+end
+
+local pinCount = 1
+local pinStorage = {}
+function addon:GeneratePinList()
+	for i = 1, #pinTable do
+		local data = pinTable[i]
+		local pinName = "TMPin"..pinCount
+		pinCount = pinCount + 1
+		local pin = CreatePin(pinName)
+		pin.name = data.name
+		pin.id = data.id
+		pin.x, pin.y = data.x, data.y
+		pinStorage[#pinStorage+1] = pin
+
+		-- If several pins are supposed to come from the same source (instance, ...), groups will anchor pins next to each other
+		if data.group then
+			local group = GetPinGroup(data.group)
+			group[#group+1] = pin
+			pin.group = data.group
+			--if #group > 1 then
+			--	data.y = data.y + 2 * (#group - 1)
+			--end
+			--UpdatePinGroupAnchors(data.group)
+			--HBDPins:AddWorldMapIconMF(addonName, group, data.id, nil, data.x/100, data.y/100)
+		end
+
+		HBDPins:AddWorldMapIconMF(addonName, pin, data.id, nil, data.x/100, data.y/100)
+	end
+	
+	--Grouped pins will be on top of each other unless we space them out
+	for name, group in pairs(pinGroupStorage) do
+		addon:UpdateGroupedPinPosition(name)
+	end
+end
+
+local oldMapID
+function addon:WORLD_MAP_UPDATE()
+	if not WorldMapButton:IsVisible() then return end
+	local continentIndex, continentID = GetCurrentMapContinent()
+	local mapID = GetCurrentMapAreaID()
+	-- We don't need to this to trigger continuously while it's still on the same map.
+	if mapID == oldMapID then return end
+	oldMapID = mapID
+end
+
+function addon:GenerateContinentList()
+	-- GetMapContinents returns a list alternating between IDs and localizzed names, so we can go in steps of two.
+	local returnList = {GetMapContinents()}
+	for i = 1, #returnList, 2 do
+		local id = returnList[i]
+		continentList[id] = returnList[i+1]
+	end
+end
+
 
 
 -------------------
@@ -45,6 +201,7 @@ local function LoadOptions()
 				type = "group",
 				name = "General",
 				args = {
+				},
 			},
 			--end of general options
 		},
@@ -68,8 +225,38 @@ function addon:OnInitialize()
 	addon:SetupOptions()
 end
 
+-- Event to care about: 
 function addon:OnEnable()
+	addon:RegisterEvent("WORLD_MAP_UPDATE")
+	addon:GenerateContinentList()
+	addon:GeneratePinList()
 end
+
+--[[
+	local function getNewPin()
+	local pin = next(pinCache)
+	if pin then
+		pinCache[pin] = nil -- remove it from the cache
+		return pin
+	end
+	-- create a new pin
+	pinCount = pinCount + 1
+	pin = CreateFrame("Button", "HandyNotesPin"..pinCount, WorldMapButton)
+	pin:SetFrameLevel(5)
+	pin:EnableMouse(true)
+	pin:SetWidth(12)
+	pin:SetHeight(12)
+	pin:SetPoint("CENTER", WorldMapButton, "CENTER")
+	local texture = pin:CreateTexture(nil, "OVERLAY")
+	pin.texture = texture
+	texture:SetAllPoints(pin)
+	pin:RegisterForClicks("LeftButtonDown", "LeftButtonUp", "RightButtonDown", "RightButtonUp")
+	pin:SetMovable(true)
+	pin:Hide()
+	return pin
+end
+
+]]
 
 function addon:ChatCommand(input)
 	InterfaceOptionsFrame_OpenToCategory("TreasureMap")
