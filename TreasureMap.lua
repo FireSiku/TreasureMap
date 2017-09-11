4594e982561586a96f2c717ae6d4161f06e26930
FireSiku <chaoslux@gmail.com>
2017-09-11 05:50:21 -0400

    RaidMounts: Added Level 60 to 90 raid mounts.

--- TreasureMap.lua
+++ TreasureMap.lua
@@ -33,11 +33,57 @@ addon.pinList = {}
 -- Addon Functions --
 ---------------------
 
+addon.pinList["Heroic Dungeon Mounts"] = {
+	{ x = 65.8, y = 48.9, id = 161, zone = 521, item = 43951, group = "COT", boss = "Infinite Corruptor"    , }, 
+	{ x = 60.2, y = 27.5, id = 499, zone = 798, item = 35513, group = "MGT", boss = "Kael'Thas Sunstrider"  , }, 
+	{ x = 41.9, y = 65.8, id = 478, zone = 723, item = 32768, group = "SH" , boss = "Anzu"                  , }, 
+	{ x = 58.4, y = 45.0, id = 491, zone = 524, item = 44151, group = "UP" , boss = "Skadi the Ruthless"    , }, 
+	{ x = 67.3, y = 32.6, id = 37 , zone = 793, item = 68824, group = "ZG" , boss = "High Priestess Kilnara", }, 
+	{ x = 67.3, y = 32.6, id = 37 , zone = 793, item = 68823, group = "ZG" , boss = "Bloodlord Mandokir"    , }, 
+	{ x = 76.1, y = 64.0, id = 463, zone = 781, item = 69747, group = "ZA" , note = "Timed Run"             , }, 
+}
+addon.pinList["Dungeon Mounts"] = {
+	{ x = 26.9, y = 11.8, id = 23 , zone = 765, item = 13335, group = "Strat", boss = "Lord Aurius Rivendare", }, 
+	{ x = 46.8, y = 49.7, id = 640, zone = 768, item = 63043, group = "SC"   , boss = "Slabhide"             , }, 
+	{ x = 75.0, y = 85.0, id = 720, zone = 769, item = 63040, group = "VP"   , boss = "Altairus"             , },  
+}
+
 addon.pinList["Raid Mounts"] = {
-	{ x = 60.2, y = 27.5, id = 499, zone = 798, item = 35513, group = "MGT" , boss = "Kael'Thas Sunstrider", },
-	{ x = 41.9, y = 65.8, id = 478, zone = 723, item = 32768, group = "Sethekk" , boss = "Anzu", },
-	{ x = 58.4, y = 45, id = 491, zone = 524, item = 44151, group = "UP" , boss = "Skadi the Ruthless", },
-	{ x = 53, y = 86, id = 492, zone = 492, item = 43951, group = "COT" , boss = "Infinite Corruptor", },
+	-- Level 60
+	{ x = 23.1, y = 86.3, id = 261, zone = 765, item = 21218, group = "AQ40", note = "Drops from trash mobs.", }, 
+	{ x = 23.1, y = 86.3, id = 261, zone = 765, item = 21323, group = "AQ40", note = "Drops from trash mobs.", }, 
+	{ x = 23.1, y = 86.3, id = 261, zone = 765, item = 21324, group = "AQ40", note = "Drops from trash mobs.", }, 
+	{ x = 23.1, y = 86.3, id = 261, zone = 765, item = 21321, group = "AQ40", note = "Drops from trash mobs.", }, 
+	-- Level 70
+	{ x = 47.1, y = 72.4, id = 32 , zone = 799, item = 30480, group = "Kara", boss = "Atuumen the Huntsman", }, 
+	{ x = 76.6, y = 65.8, id = 479, zone = 782, item = 32458, group = "Eye" , boss = "Kael'Thas Sunstrider", }, 
+	-- Level 80
+	{ x = 28.1, y = 28.1, id = 486, zone = 527, item = 43952, group = "EoE", boss = "Malygos"              , }, 
+	{ x = 28.1, y = 28.1, id = 486, zone = 527, item = 43953, group = "EoE", boss = "Malygos"              , }, 
+	{ x = 51.2, y = 78.8, id = 141, zone = 718, item = 49636, group = "Ony", boss = "Onyxia"               , }, 
+	{ x = 60.0, y = 56.0, id = 486, zone = 531, item = 43986, group = "WRT", boss = "Sartharion"           , note = "3 Drakes. 10man"      , }, 
+	{ x = 60.0, y = 56.0, id = 486, zone = 531, item = 43954, group = "WRT", boss = "Sartharion"           , note = "3 Drakes. 25man"      , }, 
+	{ x = 41.5, y = 16.1, id = 495, zone = 529, item = 45693, group = "Uld", boss = "Yogg-Saron"           , note = "0 Keepers. 25man Only", }, 
+	{ x = 53.4, y = 85.8, id = 492, zone = 604, item = 50818, group = "ICC", boss = "The Lich King (25man)", }, 
+	{ x = 49.8, y = 18.2, id = 501, zone = 532, item = 44083, group = "ICC", boss = "All Four Bosses"      , }, 
+	-- Level 85
+	{ x = 36.3, y = 84.8, id = 720, zone = 773, item = 63041, group = "TFW", boss = "Al'akir"           , }, 
+	{ x = 47.5, y = 79.6, id = 606, zone = 800, item = 71665, group = "FL" , boss = "Alysrazor"         , }, 
+	{ x = 47.5, y = 79.6, id = 606, zone = 800, item = 69224, group = "FL" , boss = "Ragnaros"          , }, 
+	{ x = 65.8, y = 48.9, id = 161, zone = 824, item = 78919, group = "COT", boss = "Ultraxion"         , }, 
+	{ x = 65.8, y = 48.9, id = 161, zone = 824, item = 77067, group = "COT", boss = "Deathwing"         , }, 
+	{ x = 65.8, y = 48.9, id = 161, zone = 824, item = 77069, group = "COT", boss = "Deathwing (Heroic)", }, 
+	-- Level 90 World Bosses
+	{ x = 67.6, y = 74.6, id = 809, item = 87771, boss = "Sha of Anger", }, 
+	{ x = 71.6, y = 64.4, id = 807, item = 89783, boss = "Galleon"     , }, 
+	{ x = 50.6, y = 54.4, id = 929, item = 94228, boss = "Oondasta"    , }, 
+	{ x = 60.5, y = 37.3, id = 928, item = 95057, boss = "Nalak"       , }, 
+	-- Level 90
+	{ x = 60.0, y = 40.0, id = 809, zone = 896, item = 87777 , group = "MSV", boss = "Elegon"                     , }, 
+	{ x = 63.0, y = 32.0, id = 928, zone = 930, item = 93666 , group = "TOT", boss = "Horridon"                   , }, 
+	{ x = 63.0, y = 32.0, id = 928, zone = 930, item = 95059 , group = "TOT", boss = "Ji-Kun"                     , }, 
+	{ x = 74.0, y = 44.0, id = 811, zone = 953, item = 104253, group = "SOO", boss = "Garrosh Hellscream (Mythic)", },  
+	
 }
 
 addon.pinList["Raiding With Leashes"] = {
@@ -79,23 +125,23 @@ addon.pinList["Raiding With Leashes"] = {
 	{ x = 40.4, y = 46.4, id = 499, zone = 789, petID = 90214, group = "SWP", boss = "The Eredar Twins"      , }, 
 	{ x = 40.4, y = 46.4, id = 499, zone = 789, petID = 90213, group = "SWP", boss = "M'uru"                 , }, 
 	-- IV: Wrath of the Lick King
-	{ x = 41.5, y = 16.1, id = 495, zone = 529, petID = 115138, group = "Ulduar", boss = "Ignis the Furnace Master", }, 
-	{ x = 41.5, y = 16.1, id = 495, zone = 529, petID = 115139, group = "Ulduar", boss = "Razorscale"              , }, 
-	{ x = 41.5, y = 16.1, id = 495, zone = 529, petID = 115140, group = "Ulduar", boss = "The Assembly of Iron"    , }, 
-	{ x = 41.5, y = 16.1, id = 495, zone = 529, petID = 115141, group = "Ulduar", boss = "Auriaya"                 , }, 
-	{ x = 41.5, y = 16.1, id = 495, zone = 529, petID = 115142, group = "Ulduar", boss = "Hodir"                   , }, 
-	{ x = 41.5, y = 16.1, id = 495, zone = 529, petID = 115143, group = "Ulduar", boss = "Freya"                   , }, 
-	{ x = 41.5, y = 16.1, id = 495, zone = 529, petID = 115144, group = "Ulduar", boss = "Mimiron"                 , }, 
-	{ x = 41.5, y = 16.1, id = 495, zone = 529, petID = 115145, group = "Ulduar", boss = "Yogg-Saron"              , }, 
-	{ x = 75.2, y = 21.9, id = 492, zone = 543, petID = 115135, group = "TOGC"  , boss = "The Northrend Beasts"    , }, 
-	{ x = 75.2, y = 21.9, id = 492, zone = 543, petID = 115136, group = "TOGC"  , boss = "The Northrend Beasts"    , }, 
-	{ x = 75.2, y = 21.9, id = 492, zone = 543, petID = 115137, group = "TOGC"  , boss = "Anub'arak"               , }, 
-	{ x = 53.4, y = 85.8, id = 492, zone = 604, petID = 115146, group = "ICC"   , boss = "Lord Marrowgar"          , }, 
-	{ x = 53.4, y = 85.8, id = 492, zone = 604, petID = 115147, group = "ICC"   , boss = "Deathbringer Saurfang"   , }, 
-	{ x = 53.4, y = 85.8, id = 492, zone = 604, petID = 115148, group = "ICC"   , boss = "Professor Putricide"     , }, 
-	{ x = 53.4, y = 85.8, id = 492, zone = 604, petID = 115149, group = "ICC"   , boss = "Sindragosa"              , }, 
-	{ x = 53.4, y = 85.8, id = 492, zone = 604, petID = 115150, group = "ICC"   , boss = "The Lich King (Normal)"  , }, 
-	{ x = 53.4, y = 85.8, id = 492, zone = 604, petID = 115152, group = "ICC"   , boss = "The Lich King (Heroic)"  , },
+	{ x = 41.5, y = 16.1, id = 495, zone = 529, petID = 115138, group = "Uld" , boss = "Ignis the Furnace Master", }, 
+	{ x = 41.5, y = 16.1, id = 495, zone = 529, petID = 115139, group = "Uld" , boss = "Razorscale"              , }, 
+	{ x = 41.5, y = 16.1, id = 495, zone = 529, petID = 115140, group = "Uld" , boss = "The Assembly of Iron"    , }, 
+	{ x = 41.5, y = 16.1, id = 495, zone = 529, petID = 115141, group = "Uld" , boss = "Auriaya"                 , }, 
+	{ x = 41.5, y = 16.1, id = 495, zone = 529, petID = 115142, group = "Uld" , boss = "Hodir"                   , }, 
+	{ x = 41.5, y = 16.1, id = 495, zone = 529, petID = 115143, group = "Uld" , boss = "Freya"                   , }, 
+	{ x = 41.5, y = 16.1, id = 495, zone = 529, petID = 115144, group = "Uld" , boss = "Mimiron"                 , }, 
+	{ x = 41.5, y = 16.1, id = 495, zone = 529, petID = 115145, group = "Uld" , boss = "Yogg-Saron"              , }, 
+	{ x = 75.2, y = 21.9, id = 492, zone = 543, petID = 115135, group = "TOGC", boss = "The Northrend Beasts"    , }, 
+	{ x = 75.2, y = 21.9, id = 492, zone = 543, petID = 115136, group = "TOGC", boss = "The Northrend Beasts"    , }, 
+	{ x = 75.2, y = 21.9, id = 492, zone = 543, petID = 115137, group = "TOGC", boss = "Anub'arak"               , }, 
+	{ x = 53.4, y = 85.8, id = 492, zone = 604, petID = 115146, group = "ICC" , boss = "Lord Marrowgar"          , }, 
+	{ x = 53.4, y = 85.8, id = 492, zone = 604, petID = 115147, group = "ICC" , boss = "Deathbringer Saurfang"   , }, 
+	{ x = 53.4, y = 85.8, id = 492, zone = 604, petID = 115148, group = "ICC" , boss = "Professor Putricide"     , }, 
+	{ x = 53.4, y = 85.8, id = 492, zone = 604, petID = 115149, group = "ICC" , boss = "Sindragosa"              , }, 
+	{ x = 53.4, y = 85.8, id = 492, zone = 604, petID = 115150, group = "ICC" , boss = "The Lich King (Normal)"  , }, 
+	{ x = 53.4, y = 85.8, id = 492, zone = 604, petID = 115152, group = "ICC" , boss = "The Lich King (Heroic)"  , },
 	-- V: Cuteaclysm
 	{ x = 20.5, y = 32.8, id = 29 , zone = 754, petID = 127850, group = "BRM", boss = "Omnotron Defense System", }, 
 	{ x = 20.5, y = 32.8, id = 29 , zone = 754, petID = 127852, group = "BRM", boss = "Maloriak"               , }, 
